8ecd041de6b5809c4830bc5fda7fe3722a5fc542
Vladiverev <vladiverev@gmail.com>
2018-06-04 11:27:05 +0300

    first testz

--- app.py
+++ app.py
@@ -1,24 +1,102 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+from flask_bcrypt import Bcrypt
 import os
 
-
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "users.login"
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-from models import Result
-
-
-@app.route('/')
-def main():
-    return render_template('index.html')
-
-@app.route('/admin')
-def showAddBlog():
-    return render_template('admin.html')
+bcrypt =Bcrypt(app)
 
 
-if __name__ == '__main__':
-    app.run()
+from form import LoginForm, RegisterForm, MessageForm
+from models import Result, User, BlogPost
+
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+# @login_required
+def home():
+    error = None
+    form = MessageForm(request.form)
+    if form.validate_on_submit():
+        new_message = BlogPost(
+            form.title.data,
+            form.description.data,
+            current_user.id
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        flash('New entry was successfully posted. Thanks.')
+        return redirect(url_for('home'))
+    else:
+        posts = db.session.query(BlogPost).all()
+        return render_template('hello.html', posts=posts, form=form, error=error)
+
+
+@app.route('/welcome')
+# @login_required
+def welcome():
+    return render_template('hello.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    error = None
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = User.query.filter_by(name = request.form['username']).first() #search in db user
+            if user is not None and \
+            bcrypt.check_password_hash(user.password,request.form['password']):
+                #session['logged_in'] = True
+                login_user(user)#check if login instead using session
+                flash('You were logged in.')
+                return redirect(url_for('home'))
+            else:
+                error = 'Invalid Credentials. Please try again.'
+                render_template('login.html', form=form,error=error)
+    return render_template('login.html', form=form,error=error)
+
+
+@app.route('/logout')
+# @login_required
+def logout():
+    logout_user()
+    flash('You were logged out.')
+    return redirect(url_for('home'))
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(
+            name=form.username.data,
+            email=form.email.data,
+            password=form.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        flash("You were successfully registered")
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form)
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter(User.id == int(user_id)).first()
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -1,6 +1,12 @@
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
 
+from app import bcrypt
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+
 
 class Result(db.Model):
     __tablename__ = 'results'
@@ -17,3 +23,54 @@ class Result(db.Model):
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
+
+
+class BlogPost(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    author_id = db.Column(db.Integer, ForeignKey('users.id'))
+
+    def __init__(self, title, description, author_id):
+        self.title = title
+        self.description = description
+        self.author_id = author_id
+
+    def __repr__(self):
+        return "<{} - {}>".format(self.title, self.description)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    __table_args__ = {'extend_existing': True}
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    posts = relationship('BlogPost', backref='author')
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def __repr__(self):
+        return "<name: {}>".format(self.name)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+
+    # def __repr__(self):
+    #     return '<name {}'.format(self.name)
